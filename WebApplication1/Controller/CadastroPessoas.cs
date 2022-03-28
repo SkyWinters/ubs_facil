dae79bdf45def92f8f7528648daf4368ebe7587e
VICTOR UZIEL TEIXEIRA ALBUQUERQUE <1550782011019@fatecitaqua.local>
2022-03-28 09:28:18 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controller/CadastroPessoas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using WebApplication1.Model;
+namespace WebApplication1.Controller
+{
+    public class CadastroPessoas
+    {
+        Conexao conexao = new Conexao();
+        SqlCommand cmd = new SqlCommand();
+        public String mensagem = "";
+
+        public string CadastroPessoa(/*string nome, string cpf*/)
+        {
+            string retorno = cmd.CommandText = "SELECT * FROM TB_ADMIN"; 
+
+            //cmd.Parameters.AddWithValue("",);
+            try
+            {
+                cmd.Connection = conexao.conectar();
+                cmd.ExecuteNonQuery();
+                conexao.desconectar();
+                this.mensagem = "Cadastrado com Sucesso";
+            }
+            catch (SqlException e)
+            {
+                this.mensagem = "Erro ao conectar com BD";
+            }
+            return retorno;
+
+        }
+    }
+}
---
+++ WebApplication1/Model/Conexao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Model
+{
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+
+        public Conexao()
+        {
+            con.ConnectionString = "SERVER=backendaap.ddns.net; DATABASE=aapv; UID=geral; PWD=aapv; PORT=3306";
+
+        }
+        public SqlConnection conectar()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+
+        public SqlConnection desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Close();
+            }
+            return con;
+        }
+    }
+}
